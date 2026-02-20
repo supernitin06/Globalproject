0cc0df7bb47545fb2579ff776360adb40d3c32dc
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2026-02-20 14:48:49 +0530

    nain

--- config/db.js
+++ config/db.js
@@ -12,3 +12,16 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+New Message
+02:45 PM
+PORT= 5001
+MONGO_URI=mongodb+srv://divyasingh:divyasingh0612@cluster0.hjvd31d.mongodb.net/formSubmit?retryWrites=true&w=majority&appName=Cluster0
+JWT_SECRET=blobalProjectbimaxtechnolog
+EMAIL_USER=no-reply@projectsglobal.in
+EMAIL_PASS= Noreply@123580
+SMTP_HOST=mail.projectsglobal.in
+SMTP_PORT=587
+CLOUDINARY_CLOUD_NAME=dlhp3v3fd
+CLOUDINARY_API_KEY=897159174793714
+CLOUDINARY_API_SECRET=QPkyvV6Q7Q00lhbfk7LcuVBi8EE
